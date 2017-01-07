d50d9146f21032fa96c15a141e76907f637444a2
root <root@miszcz>
2017-01-07 00:43:59 +0000

    Fix issue with user home directory

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker run -v /home/miszcz:/home/developer --name cloudsys -it terraform-managment 
+HOST_USER_ID=$(id -u)
+: ${USER_NAME:="developer"}
+if [ -n "$1" ]; then
+  USER_NAME=$1
+fi
+echo $USER_NAME
+docker run -v /home/$USER_NAME:/home/developer --name cloudsys -it terraform-managment 
 # Clean 
-docker ps -a | grep -e Exit -e cloudsys| cut -d ' ' -f 1 | xargs sudo docker rm
+docker ps -a | grep -e Exit -e cloudsys | cut -d ' ' -f 1 | xargs sudo docker rm
