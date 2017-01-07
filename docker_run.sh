880ac2027cda43c2952d6ea311885603748f0149
mistrzek <marcintaracha1991@gmail.com>
2017-01-07 00:44:50 +0000

    Fix user issue

--- docker_run.sh
+++ docker_run.sh
@@ -5,7 +5,7 @@ HOST_USER_ID=$(id -u)
 if [ -n "$1" ]; then
   USER_NAME=$1
 fi
-echo $USER_NAME
+
 docker run -v /home/$USER_NAME:/home/developer --name cloudsys -it terraform-managment 
 # Clean 
 docker ps -a | grep -e Exit -e cloudsys | cut -d ' ' -f 1 | xargs sudo docker rm
